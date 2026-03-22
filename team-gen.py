528dcfafb794bacf1d3baeb417efbb3c7be72783
anacondalivin <antaviusf13@gmail.com>
2026-03-22 16:04:50 -0700

    add basketball player team lookup using conditionals and lower()

---
+++ team-gen.py
@@ -0,0 +1,16 @@
+name = input("Whats there name?").lower()
+
+if name == "kobe":
+    print("Lakers")
+elif name =="kd":
+    print("Rockets")
+elif name == "shai":
+    print("Okc")
+elif name == "jb":
+    print("Celtics")
+elif name =="cade":
+    print("Pistons")
+else:
+    print("Dont know him, must be no good cause i know everybody")
+
+
