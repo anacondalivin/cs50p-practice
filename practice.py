0600460f38e682061639f750ec67d9d35a87cf36
anacondalivin <antaviusf13@gmail.com>
2026-03-22 08:43:24 -0500

    Add files via upload
    Variable practice

---
+++ practice.py
@@ -0,0 +1,25 @@
+
+# variable = a resusable container for storing a value 
+#            a variable behaves as if it were value it contains 
+
+# intergers
+age = 25
+players = 210
+quantity = 3 
+
+print(f"You are {age} years old")
+print(f"there are {players} players online")
+print(f"You would like to buy {quantity} items")
+
+
+print("You are " + str(age) + " years old")
+print("You are", age, "years old")
+
+# floats
+gpa = 4.0
+classes = 4
+tuition = 4000.00
+
+print(f"Your gpa is {gpa}")
+print(f"You have {classes} classes")
+print(f"Your tuition total is {tuition}")
